4034b18e578e7eb18143404054ccaca771ccb4a8
Paul Shumeika <knux.wild@gmail.com>
2013-10-24 11:17:33 +0300

    f*cked up a bit, recommit

--- echo-server.rb
+++ echo-server.rb
@@ -1,1 +1,20 @@
 require 'socket'
+
+port = 2023
+address = '127.0.0.1'
+
+server = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
+addrinfo = Socket.sockaddr_in(port, address)
+server.bind(addrinfo)
+server.listen(1)
+
+client = server.accept  # client[0] - socket descriptor, client[1] - Addrinfo 
+
+    loop do
+      data = client[0].recv(1488)
+
+      break if data.empty?
+      client[0].send(data, 0)
+    end
+
+ server.close 
