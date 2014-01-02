3fae78a043cf261ad8fb49f7a476f0ac4a9631c7
Paul Shumeika <knux.wild@gmail.com>
2014-01-03 00:50:10 +0300

    Server major fix

--- FTransfer/tr_server.rb
+++ FTransfer/tr_server.rb
@@ -10,14 +10,14 @@ end
 
 address = ARGV[0]  #server side params
 port = ARGV[1].to_i
-MTU = 1400
+block_size = 1400
 PR_size = 1024 # hardcoded protocol mesage size
 file_name = ""
 current_block = 0
 state = :ready #ready or aborted
 
 server = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM) #oppening connection section
-addr = Addrinfo.tcp(address, port)
+addr = Addrinfo.tcp(address, port)  #sockaddr structure
 server.bind(addr)
 server.listen(1)
 client_info = server.accept  # client[0] - socket descriptor, client[1] - Addrinfo 
@@ -31,20 +31,24 @@ p "State is sent"
 file_description = client.recv(PR_size)
 p "File description: #{file_description}"
 file_description = file_description.split("::")
-# file_description[0] - file_name ; file_description[1] - size; file_description[2] - MTU;
+# file_description[0] - file_name ; file_description[1] - size; file_description[2] - block_size;
 # file_description[3] - blocks_num; file_description[4] - timeout
 file_name = file_description[0]
 file_size = file_description[1].to_i
-MTU = file_description[2].to_i
+block_size = file_description[2].to_i
 blocks_num = file_description[3].to_i
 timeout = file_description[4].to_i
 
 File.open(file_name,"w") do |file|
-  while (current_block < blocks_num) do
+  while (current_block < blocks_num-1) do
   	begin 
       status = Timeout::timeout(timeout) do
-        packet = client.recv(MTU,0)
-        file.write(packet)
+        packet = client.recv(block_size,0)
+          while packet.length < block_size do                          # This is needed to be sure, that blocks
+            packet = packet + client.recv(block_size-packet.length,0)  # with block_size (not less) are written
+          end                                                          # the file
+        num = file.write(packet)
+        print "."
         current_block = current_block + 1
       end
     rescue Timeout::Error
