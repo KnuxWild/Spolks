3c9bbb0f66199c981ab6b0c1f85aa9621783938b
Paul Shumeika <knux.wild@gmail.com>
2014-01-03 05:11:50 +0300

    Server final version

--- FTransfer/tr_server.rb
+++ FTransfer/tr_server.rb
@@ -14,19 +14,20 @@ block_size = 1400
 PR_size = 1024 # hardcoded protocol mesage size
 file_name = ""
 current_block = 0
+last_packet = ""
 state = :ready #ready or aborted
 
 server = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM) #oppening connection section
+server.setsockopt(Socket::SOL_SOCKET,Socket::SO_REUSEADDR, true)
 addr = Addrinfo.tcp(address, port)  #sockaddr structure
 server.bind(addr)
 server.listen(1)
 client_info = server.accept  # client[0] - socket descriptor, client[1] - Addrinfo 
 client = client_info[0] # client socket 
-
-server_state = state.to_s + "::" + file_name + "::" + current_block.to_s
-
+p "Connection established"
+server_state = state.to_s + "::" + file_name + "::" + current_block.to_s + "::" + block_size.to_s
 client.send(server_state,0)
-p "State is sent"
+p "Server state is sent"
 
 file_description = client.recv(PR_size)
 p "File description: #{file_description}"
@@ -47,16 +48,79 @@ File.open(file_name,"w") do |file|
           while packet.length < block_size do                          # This is needed to be sure, that blocks
             packet = packet + client.recv(block_size - packet.length,0)  # with block_size (not less) are written to
           end                                                          # the file
-        num = file.write(packet)
+        file.write(packet)
         print "."
         current_block = current_block + 1
       end
     rescue Timeout::Error
       p "Connection seems to be aborted."
       state = :aborted
+      file.close
       break
     end
   end
+ 
+  last_packet_size = file_size - block_size * (blocks_num-1) # Write the last portion of data which is less than block_size
+  if state == :ready
+    while last_packet.length < last_packet_size do
+      packet = client.recv(block_size,0)
+      last_packet = last_packet + packet
+    end
+    file.write(last_packet)
+  end  
+  server.close
+end
+
+
+# Second part of server which will wait until file will be fully reuploaded
+
+while state == :aborted do # Reuploading file after disconnect
+  p "Waiting for file reuploading"
+
+  server = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM) #oppening connection section
+  server.setsockopt(Socket::SOL_SOCKET,Socket::SO_REUSEADDR, true)
+  addr = Addrinfo.tcp(address, port)  #sockaddr structure
+  server.bind(addr)
+  server.listen(1)
+  client_info = server.accept  # client[0] - socket descriptor, client[1] - Addrinfo 
+  client = client_info[0] # client socket 
+  p "Connection established"
+  server_state = state.to_s + "::" + file_name + "::" + current_block.to_s + "::" + block_size.to_s
+  client.send(server_state,0)
+  p "Server state is sent"
+  state = :ready
+
+  File.open(file_name,"a") do |file|
+    while (current_block < blocks_num - 1) do
+      begin 
+        status = Timeout::timeout(timeout) do
+          packet = client.recv(block_size,0)
+            while packet.length < block_size do                            # This is needed to be sure, that blocks
+              packet = packet + client.recv(block_size - packet.length,0)  # with block_size (not less) are written to
+            end                                                            # the file
+          file.write(packet)
+          print "."
+          current_block = current_block + 1
+        end
+      rescue Timeout::Error
+        p "Connection seems to be aborted."
+        state = :aborted
+        file.close
+        break
+      end
+    end
+    
+    last_packet_size = file_size - block_size * (blocks_num-1) # Write the last portion of data which is less than block_size
+    if state == :ready
+      p "Im here, sucker"
+      while last_packet.length < last_packet_size do
+        packet = client.recv(block_size,0)
+        last_packet = last_packet + packet
+      end
+      file.write(last_packet)
+    end  
+  end
+  server.close
 end
 
 
